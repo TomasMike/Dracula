5a0049b7d77c77911de473df77ee38f4ae2c690d
Tomas Mike <tomasmike182@gmail.com>
2019-12-31 06:10:40 +0100

    .

---
+++ Dracula.Web.NET/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Dracula.Web.NET
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Dracula.Web.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dracula.Web.NET.MvcApplication" Language="C#" %>
