1cdcd94685187a07ae745bc84d91f95ec640d845
Tomas Mike <tomasmike182@gmail.com>
2020-05-26 15:32:00 +0200

    .

--- Dracula.Web/Game.cs
+++ Dracula.Web/Game.cs
@@ -2,25 +2,85 @@
 
 namespace Dracula.Web
 {
-	public class Game
-	{
-		public List<Player> Players;
-	}
+    public class Game
+    {
+        public Game()
+        {
+           
+        }
 
-	public class Player
-	{
-		public string Name;
-		public int TopOffset;
-		public int LeftOffset;
-		public Character? SelectedCharacter;
-	}
+        private void InitLocations()
+        {
+            Locations = new List<Location>()
+            {
+                new Location("",LocationType.LargeCity),
 
-	public enum Character
-	{
-		Dracula,
-		LordGodalming,
-		DrJohnStewart,
-		VanHelsing,
-		MinaHarker,
-	}
+            };
+        }
+
+        private List<Player> Players;
+        private List<Location> Locations;
+        private int Influence;
+        private object Time;
+    }
+
+    public class Player
+    {
+        public string Name;
+        public int TopOffset;
+        public int LeftOffset;
+        public Character? SelectedCharacter;
+    }
+
+    public enum Character
+    {
+        Dracula,
+        LordGodalming,
+        DrJohnStewart,
+        VanHelsing,
+        MinaHarker,
+    }
+
+    public class Location
+    {
+        public Location(string name, LocationType locationType)
+        {
+            Name = name;
+            LocationType = locationType;
+        }
+
+        public string Name;
+        public List<(Location, LocationConnectionType)> ConnectedLocations;
+        public LocationType LocationType;
+    }
+
+
+
+    public enum LocationType
+    {
+        LargeCity,
+        SmallCity,
+        SeaZone
+    }
+
+    public enum LocationConnectionType
+    {
+        Road,
+        RailwayWhite,
+        RailwayYellow
+    }
+
+    public enum MapZone
+    {
+        SpainPortugal,
+        France,
+        Italy,
+        GreatBritain,
+        GreeceRomania,
+        HungaryCroatiaCzechSlovakia,
+        Germany,
+    }
+
+
+
 }
